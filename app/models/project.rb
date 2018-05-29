79b9c44f3c7cb1cc40dce525ef359cd89f9163d8
beranemo <beranemo@gmail.com>
2018-05-30 00:09:22 +0800

    custom message of length

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
-  validates :title, presence: { message: '不能為空' }
+  validates(
+    :title, 
+    presence: { message: '不能為空' }, 
+    length: { minimum: 3, message: '最少長度要 3'})
 end
